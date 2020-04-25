d27d687ed6d0e91f066f61ada3f379e8abe7e6fa
Kamila <kamilla8@o2.pl>
2020-04-25 11:07:54 +0100

    js in login

--- js/login.js
+++ js/login.js
@@ -1,31 +1,42 @@
 function checkPassword(event) {
 
-    var userisLoggedin = false;
-    var x = document.getElementById("userPassword").value;
-    var y = document.getElementById("userEmail").value;
+    var invalidFields = false;
+    var userPassword = document.getElementById("userPassword").value;
+    var userEmail = document.getElementById("userEmail").value;
 
     document.getElementById("passwordMessage").style.display = "none";
-    
+    document.getElementById("emailMessage").style.display = "none";
 
-    if (x == "" || y == "") {
+    // Password validation
+    if (userPassword == "") {
         //alert("Error: Password cannot be blank!");
         document.getElementById("passwordMessage").style.display = "Block";
-        alert("Please log in first");
-    } else if (x.length < 6) {
+        //alert("Please log in first");
+        invalidFields = true;
+    } else if (userPassword.length < 6) {
         //alert("Error: Password must contain at least 6 characters");
         document.getElementById("passwordMessage").style.display = "Block";
-    } else {
-        userisLoggedin = true;
+        invalidFields = true;
     }
 
-    if (userisLoggedin == true) {
+    //Email validation
+    if (userEmail == "") {
+        document.getElementById("emailMessage").style.display = "Block";
+        invalidFields = true;
+    }
+
+    if (invalidFields == false) {
+        // you are to go
+        // login modal goes away
         document.getElementById("loginButton").style.display = "none";
         document.getElementById("buyNowButton").style.display = "Block";
         document.getElementById("userLoggedIn").style.display = "Block";
-        document.getElementById("userLoggedIn").innerText = y;
-        document.getElementById("userLoggedIn").innerText = "Logged in"+" "+ " "+y;
-
-        //<span class="navbar-text">Logged In</span>
+        document.getElementById("userLoggedIn").innerText = userEmail;
+        document.getElementById("userLoggedIn").innerText = userEmail + " logged in"
+    }
+    else{
+        // we have invalids
+        // login modal stays
     }
 }
 
