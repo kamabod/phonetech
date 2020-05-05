974e07f88aa734684af4fd1cb587b4c8e7b9e09c
Kamila <kamilla8@o2.pl>
2020-05-05 09:12:06 +0100

    improv

--- js/randomimages.js
+++ js/randomimages.js
@@ -2,7 +2,7 @@
 window.onload = choosePic;
 
 var myPics = new Array("Images/smartphones.jpg", "Images/phoneOnDesk.jpg", "Images/selfies.jpg", "Images/phoneInFlowers.jpg",
- "Images/phoneAtConcert.jpg");
+ "Images/phoneOnBeach.jpg");
 var uniqueNumbers = new Array(3);
 
 function choosePic() {
